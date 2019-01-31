4422d86774005f5824549796719ea3b1dd4bdc6f
Ryan Andri <ryanandri@linuxmail.org>
2019-02-01 04:39:45 +0700

    Revert "ANDROID: sdcardfs: Add option to drop unused dentries"
    * doesn't compatible with oreo VolumeInfo.java
    
    This reverts commit 181cfa62c7960330b63832bf5e491121df2fd802.

--- fs/sdcardfs/main.c
+++ fs/sdcardfs/main.c
@@ -34,7 +34,6 @@ enum {
 	Opt_reserved_mb,
 	Opt_gid_derivation,
 	Opt_default_normal,
-	Opt_nocache,
 	Opt_unshared_obb,
 	Opt_err,
 };
@@ -51,7 +50,6 @@ static const match_table_t sdcardfs_tokens = {
 	{Opt_default_normal, "default_normal"},
 	{Opt_unshared_obb, "unshared_obb"},
 	{Opt_reserved_mb, "reserved_mb=%u"},
-	{Opt_nocache, "nocache"},
 	{Opt_err, NULL}
 };
 
@@ -75,7 +73,6 @@ static int parse_options(struct super_block *sb, char *options, int silent,
 	/* by default, gid derivation is off */
 	opts->gid_derivation = false;
 	opts->default_normal = false;
-	opts->nocache = false;
 
 	*debug = 0;
 
@@ -132,9 +129,6 @@ static int parse_options(struct super_block *sb, char *options, int silent,
 			break;
 		case Opt_default_normal:
 			opts->default_normal = true;
-			break;
-		case Opt_nocache:
-			opts->nocache = true;
 			break;
 		case Opt_unshared_obb:
 			opts->unshared_obb = true;
